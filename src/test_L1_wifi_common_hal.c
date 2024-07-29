0f98f0d0af44ebbd9df46b057ff87a98fc704e86
DevikaJaladi <devshell123@gmail.com>
2024-07-29 16:27:16 -0400

    gh #15 Fixing postive and negative TCs of test_L1_wifi_common_hal and test_L1_wifi_client_hal

--- src/test_L1_wifi_common_hal.c
+++ src/test_L1_wifi_common_hal.c
@@ -4559,9 +4559,10 @@ void test_l1_wifi_common_hal_positive1_wifi_getRegulatoryDomain (void)
     UT_LOG("Return status: %d \n", status);
     UT_LOG("Return value: %s \n", output_string);
     UT_ASSERT_EQUAL(status, RETURN_OK); 
-    if((output_string[2] != ' ') && (output_string[2] != 'O') && (output_string[2] != 'I') && (output_string[2] != '\0'))
-    {
-        UT_LOG("Invalid 3rd octet : %c \n",output_string[2]);
+    if ((output_string[2] == ' ') || (output_string[2] == 'O') || (output_string[2] == 'I') || (output_string[2] == '\0')) {
+        UT_FAIL("Valid 3rd octet\n");
+    } else {
+        UT_LOG("Invalid 3rd octet : %c \n", output_string[2]);
         UT_FAIL("Invalid 3rd octet\n");
     }
 
